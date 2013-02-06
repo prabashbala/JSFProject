9eb59124db3af733cda24205638374e22ad75905
BCCAPHBA <BCCAPHBA@SB0073308.addm.ads.brm.pri>
2013-02-06 14:35:52 +0000

    add css style for error messages

--- src/main/java/uk/gov/bcc/birmingham/bcfaces/AuthenticationBean.java
+++ src/main/java/uk/gov/bcc/birmingham/bcfaces/AuthenticationBean.java
@@ -1,11 +1,13 @@
 package uk.gov.bcc.birmingham.bcfaces;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.SessionScoped;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.ValidatorException;
 
 @ManagedBean
-@SessionScoped
-public class LoginBean {
+public class AuthenticationBean {
 
 	/**
 	 * 
@@ -40,6 +42,20 @@ public class LoginBean {
 	public String processLogin(){
 		System.out.println("Hi world");
 		return "welcomePage";
+	}
+	
+	public void validatePassword(FacesContext context, UIComponent comp, Object values)
+			throws ValidatorException {
+
+		String password = (String) values;
+
+		if (!password.equalsIgnoreCase("8080")) {
+			FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_ERROR,
+					"login.password.notmatch", "does not match");
+			context.addMessage(comp.getClientId(context), msg);
+			throw new ValidatorException(msg);
+		}
+
 	}
 	
 	
